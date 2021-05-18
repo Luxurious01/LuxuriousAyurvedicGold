3d545fec96d2697d37c642733ae8eb63713b52f9
Luxurious01 <83532412+Luxurious01@users.noreply.github.com>
2021-05-18 10:31:59 +0530

    Add files via upload

--- page.js
+++ page.js
@@ -1,43 +1,42 @@
-/*const mongoose = require('mongoose');
-const pageSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    banners: [
-        {
-            img: { type: String },
-            navigateTo: { type: String }
-        }
-    ],
+/*const express = require('express')
+const router  = express.Router()
+const Page = require('../../models/page');
+const  upload  = require('../../middleware/upload');
 
-    products: [
-        {
-            img: { type: String },
-            navigateTo: { type: String }
-        }
-    ],
-    category: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Category',
-        required: true
-    },
-    createdBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
+router.post('/page/create', upload.fields([
+    { name: 'banners'},
+    { name: 'products'}
+]),(req,res) =>{
+    
+        
+      
+    const{banners , products } = req.files;
+    if(banners.length > 0){
+    req.body.banners   = banners.map((banner , index ) => ({
+            img: `http://localhost:5000/public/`,
+            navigateTo: `/bannerclicked?categoryId=req.body.category&type=req.body.type`
+        }));
+    }
+    if(products.length > 0){
+       req.body.products= products.map((product , index ) => ({
+            img: `http://localhost:5000/public/${products.filename}`,
+            navigateTo: `/productclicked?categoryId=req.body.category&type=req.body.type`
+        }));
     }
 
+     //req.body.createdBy = req.user._id;
+
+     const page = new Page(req.body);
+     page.save((error,page) => {
+         if(error) return res.status(400).json({ error });
+         if(page){
+             return res.status(201).json({ page });
+         }
+     })
 
 
-}, { timestamps: true });
+    res.status(200).json({ body: req.body });
+})
 
 
-
-module.exports = mongoose.model('Page', pageSchema); */
+module.exports = router */
